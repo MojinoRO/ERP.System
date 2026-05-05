019c39f21dc5c5365e7b5449c4e6a05e5a0909f0
Moji No <Mojino20@outlook.com>
2026-05-05 18:01:16 -0500

    backend

--- ERP.Application/Services/ConfSubCategoriasService.cs
+++ ERP.Application/Services/ConfSubCategoriasService.cs
@@ -1,3 +1,4 @@
+using System.Dynamic;
 using ERP.Application.DTOs;
 using ERP.Application.Interfaces;
 using ERP.Domain.Interfaces;
@@ -6,9 +7,19 @@ namespace ERP.Application.Services
 {
     public class ConfSubCategoriasService : IConfSubCategoriasService
     {
-        private readonly IConfSubCategoriasRepository _repo 
+        private readonly IConfSubCategoriasRepository _repo;
         public ConfSubCategoriasService(IConfSubCategoriasRepository reposotory) => _repo= reposotory;
 
-        public async Task<IEnumerable<ConfSubCategeriasDTOs>>
+        public async Task<IEnumerable<ConfSubCategeriasDTOs>> getAllAsync()
+        {
+            var sb = await _repo.getAllAsync();
+            return sb.Select(sb => new ConfSubCategeriasDTOs
+            {
+                SubCategoriasID=sb.SubCategoriaID,
+                CategoriaID=sb.CategoriaID,
+                SubCategoriaCodigo=sb.SubCategoriaCodigo,
+                SubCategoriasCodigo=sb.SubCategoriaNombre
+            });
+        }
     }
 }
